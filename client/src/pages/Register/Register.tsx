a2ce60d5ab4b8b7101921a3377c8982bd84511e1
kritika Bendwar <kritikabendwar123@gmail.com>
2026-08-08 19:43:14 +0530

    Structuring the register page

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,5 +1,18 @@
+import RegisterBanner from "../../components/auth/RegisterBanner";
+import RegisterForm from "../../components/auth/RegisterForm";
+
+import "../../styles/register.css";
+
 const Register = () => {
-  return <h1>Register Page</h1>;
+  return (
+    <div className="register-page">
+
+      <RegisterBanner />
+
+      <RegisterForm />
+
+    </div>
+  );
 };
 
 export default Register;
